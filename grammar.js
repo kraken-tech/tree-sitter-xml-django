0197210200f34c5519028ca5afc3ae9eac237667
Laurent Putz <laurent.putz@kraken.tech>
2026-08-28 16:21:51 +0100

    Introduce the concept of assignment inside Django tag attributes
    Prior to this change, assigning values to keys within
    {% some_django_tag x=y %} would be parsed as
            (dj_variable
              (dj_variable_name))
            (dj_variable
              (dj_variable_name))
    i.e. both sides of the '=' are parsed as variabled. However, we the
    grammar should recognise these attributes as 'django assingments'.
    
    This change extends the grammar to support this. The 'x' will henceforth
    be parsed as the 'assignment target' and the 'y' as the value. The
    latter can be a string, number, boolean, or general Django variable.

--- grammar.js
+++ grammar.js
@@ -427,6 +427,15 @@ export default grammar({
     // the multi-block case.
     dj_comment_content: $ => $._dj_comment_body_text,
 
+    // A key=value pair used in {% with key=value %}, {% include ... with key=val %},
+    // custom tags with keyword arguments, etc.  The LHS is an assignment_target —
+    // a name being bound, distinct from a variable being read.
+    dj_assignment: $ => seq(
+      field('name', alias($.dj_variable_name, $.dj_assignment_target)),
+      '=',
+      field('value', choice($.dj_variable, $.dj_string, $.dj_number, $.dj_boolean)),
+    ),
+
     _dj_attribute: $ => seq(
       choice(
         $.dj_keyword,
@@ -436,8 +445,9 @@ export default grammar({
         $.dj_boolean,
         $.dj_string,
         $.dj_variable,
+        $.dj_assignment,
       ),
-      optional(choice(',', '=')),
+      optional(','),
     ),
 
     // -------------------------------------------------------------------------
